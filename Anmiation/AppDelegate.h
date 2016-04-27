2fbebf2824f7c007b51654fb4622be3c66c86d68
koxx <912662036@qq.com>
2016-04-27 09:06:17 +0800

    Initial Commit

---
+++ Anmiation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anmiation
+//
+//  Created by Afei on 16/4/27.
+//  Copyright © 2016年 梁玉玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Anmiation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Anmiation
+//
+//  Created by Afei on 16/4/27.
+//  Copyright © 2016年 梁玉玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
